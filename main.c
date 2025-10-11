24ce20a1ac01387e5395e6e3eef73cbb211527ac
Penguin <penguinguy2550@gmail.com>
2025-10-12 00:20:36 +0300

    First commit for this file
    Got the basic structure for a future county game in C going

---
+++ main.c
@@ -0,0 +1,46 @@
+// SUPER DUPER COOL DISCLAIMER
+// This code isn't made to be used as an ACTUAL program,
+// instead, it's just a series of tests that I will use to
+// make my Counties in C game (and possibly States in C if
+// this one is a success).
+//
+// That being said, this file is also part of a repo which
+// will hold all of my C tests (and possibly other languages?).
+//
+// Cya!
+
+#include <stdio.h>
+
+int main() {
+
+    // stuff that start with the letter B
+    char counties[] = {"bed", "bear", "bored"};
+
+
+    // main menu
+    void menu() {
+        while(1) {
+            printf("County game practice program --- C test\nSelect an option:\n1.The game\n2.(this doesnt exist lol)\n> ");
+
+        }
+
+    }
+
+    // game function
+    void county_game() {
+        // defining some stuff before the actual game
+        // array for game_counties
+        char county[5];
+        char game_counties[3];
+
+        // main game
+        // main loop
+        while(1) {
+            printf("Guess a word!\n> ");
+            scanf("%s", &county);
+
+        }
+
+
+    }
+}
