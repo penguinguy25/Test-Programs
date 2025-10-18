50c046c2abd47fa5e96394f811ad0637b2b58f9c
Penguin <penguinguy2550@gmail.com>
2025-10-19 02:31:44 +0300

    initial commit, this is not finished

---
+++ binaryconverter.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+char* converter(char* str)
+{
+    // ruling out empty strings
+    if(str[0] == '\0')
+    {
+        printf("hi\n");
+    }
+    else
+    {
+
+    }
+}
+
+// main
+int main()
+{
+    printf("%s\n", converter(""));
+    return 0;
+}
