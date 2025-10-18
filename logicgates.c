a4670cabb6ab0a4cf7600c8e768ea21c784ea865
Penguin <penguinguy2550@gmail.com>
2025-10-18 20:29:50 +0300

    added some gates

---
+++ logicgates.c
@@ -0,0 +1,51 @@
+// logic gates
+#include <stdio.h>
+
+// NOT gate
+int not(int bit)
+{
+    if(bit == 1)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+// AND gate
+int and(int bit1, int bit2)
+{
+    if(bit1 == 1 && bit2 == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+// NAND gate
+int nand(int bit1, int bit2)
+{
+    if(bit1 == 1 && bit2 == 1)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+// main program execution
+int main()
+{
+
+    printf("%d\n", nand(0, 1));
+    printf("%d\n", nand(1, 1));
+
+    return 0;
+}
