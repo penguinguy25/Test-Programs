568602ece2259e29f74a30879f5fa66cc510de2a
Penguin <penguinguy2550@gmail.com>
2025-10-29 22:54:03 +0200

    uh

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+
+    int c = 0;
+    while(c < 100)
+    {
+        std::cout << "Printing Stuff" << std::endl;
+        c++; // title drop!
+
+        // ^^^ inspired from a funny code comment i saw on the internet once lol
+    }
+
+    return 0;
+}
